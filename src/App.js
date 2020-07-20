6149c4ae72916d2771e4645f5afa322e7f9f9d47
Andrey1019 <julianherrera1019@gmail.com>
2020-07-20 16:29:59 -0500

    subindo los primeros archivos

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+    <table>
+<tr>
+<th className="font">Nombres</th>
+<th className="font">Apellidos</th>
+<th className="font">Gmail</th>
+<th className="font"># Movil</th>
+</tr>
+<tr>
+  <td>Julian Andrey</td>
+  <td>Herrera Berrio</td>
+  <td>julianherrera1019@gmail.com</td>
+  <td>3008664803</td> 
+</tr>
+<tr>
+  <td>Jeses Andres</td>
+  <td>Areiza Martinez</td>
+  <td>andresareiza2016@gmail.com</td>
+  <td>3023679903</td> 
+</tr>
+<tr>
+  <td>Freiler </td>
+  <td>Ortega Ardila</td>
+  <td>ortegaardilaxd@gmail.com</td>
+  <td>3002925889</td> 
+</tr>
+<tr>
+  <td>Raysiel Asael </td>
+  <td>Cortesia Mesa</td>
+  <td></td>
+  <td>3148445573</td> 
+</tr>
+</table>
+  </div>
+);
+}
+
